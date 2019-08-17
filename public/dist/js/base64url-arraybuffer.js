4304ec7024dc1b7b76a473d2490b4ceda9c0077c
poruruba <44470781+poruruba@users.noreply.github.com>
2019-08-17 20:11:58 +0900

    Add files via upload

--- public/dist/js/base64url-arraybuffer.js
+++ public/dist/js/base64url-arraybuffer.js
@@ -8,75 +8,80 @@
  * 
  */
 (function(){
-    'use strict';
+    "use strict";
 
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
     // Use a lookup table to find the index.
-    let lookup = new Uint8Array(256);
-    for (let i = 0; i < chars.length; i++) {
+    var lookup = new Uint8Array(256);
+    for (var i = 0; i < chars.length; i++) {
         lookup[chars.charCodeAt(i)] = i;
     }
 
-    let encode = function(arraybuffer) {
-        let bytes = new Uint8Array(arraybuffer),
-        i, len = bytes.length, base64url = '';
+    var encode = function(arraybuffer) {
+        var bytes = new Uint8Array(arraybuffer),
+        i, len = bytes.length, base64 = "";
 
         for (i = 0; i < len; i+=3) {
-            base64url += chars[bytes[i] >> 2];
-            base64url += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
-            base64url += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
-            base64url += chars[bytes[i + 2] & 63];
+            base64 += chars[bytes[i] >> 2];
+            base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+            base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+            base64 += chars[bytes[i + 2] & 63];
         }
 
         if ((len % 3) === 2) {
-            base64url = base64url.substring(0, base64url.length - 1);
+            base64 = base64.substring(0, base64.length - 1);
         } else if (len % 3 === 1) {
-            base64url = base64url.substring(0, base64url.length - 2);
+            base64 = base64.substring(0, base64.length - 2);
         }
 
-        return base64url;
+        return base64;
     };
 
-    let decode = function(base64string) {
-        let bufferLength = base64string.length * 0.75,
-        len = base64string.length, i, p = 0,
+    var decode = function(base64) {
+        var bufferLength = base64.length * 0.75,
+        len = base64.length, i, p = 0,
         encoded1, encoded2, encoded3, encoded4;
 
-        let bytes = new Uint8Array(bufferLength);
+        var arraybuffer = new ArrayBuffer(bufferLength),
+        bytes = new Uint8Array(arraybuffer);
 
         for (i = 0; i < len; i+=4) {
-            encoded1 = lookup[base64string.charCodeAt(i)];
-            encoded2 = lookup[base64string.charCodeAt(i+1)];
-            encoded3 = lookup[base64string.charCodeAt(i+2)];
-            encoded4 = lookup[base64string.charCodeAt(i+3)];
+            encoded1 = lookup[base64.charCodeAt(i)];
+            encoded2 = lookup[base64.charCodeAt(i+1)];
+            encoded3 = lookup[base64.charCodeAt(i+2)];
+            encoded4 = lookup[base64.charCodeAt(i+3)];
 
             bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
             bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
             bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
         }
 
-        return bytes.buffer
+        return arraybuffer;
     };
 
-    let methods = {
-        'decode': decode,
-        'encode': encode
-    }
-
-    /**
+     /**
      * Exporting and stuff
      */
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = methods;
+        module.exports = {
+            'encode': encode,
+            'decode': decode
+        }
 
     } else {
         if (typeof define === 'function' && define.amd) {
             define([], function() {
-                return methods
+                return {
+                    'encode': encode,
+                    'decode': decode
+                }
             });
         } else {
-            window.base64url = methods;
+            window.base64url = {
+                'encode': encode,
+                'decode': decode
+            }
         }
     }
 })();

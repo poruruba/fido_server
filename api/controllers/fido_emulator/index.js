0fab97dd35adcc6cddc7148b6e425b48b1b5ee05
poruruba <44470781+poruruba@users.noreply.github.com>
2020-04-05 10:51:53 +0900

    Update index.js

--- api/controllers/fido_emulator/index.js
+++ api/controllers/fido_emulator/index.js
@@ -4,8 +4,8 @@ const HELPER_BASE = process.env.HELPER_BASE || '../../helpers/';
 const Response = require(HELPER_BASE + 'response');
 const Redirect = require(HELPER_BASE + 'redirect');
 
+const rs = require('jsrsasign');
 const ECDSA = require('ecdsa-secp256r1')
-const x509 = require('js-x509-utils');
 const crypto = require('crypto');
 
 const curveLength = Math.ceil(256 / 8);
@@ -16,6 +16,9 @@ const FIDO_EXPIRE = Number(process.env.FIDO_EXPIRE) || 365;
 
 var total_counter = Number(process.env.COUNTER_START) || 0;
 var total_application_id = Number(process.env.APPLICATION_ID_START) || 1;
+
+// X509証明書の楕円暗号公開鍵ペアの作成
+var kp_cert = rs.KEYUTIL.generateKeypair('EC', 'secp256r1');
 
 exports.handler = async (event, context, callback) => {
   if( event.path == "/device/u2f_register"){
@@ -50,40 +53,56 @@ exports.handler = async (event, context, callback) => {
 
 async function u2f_register(challenge, application){
   // 楕円暗号公開鍵ペアの作成
-  var ecdh = crypto.createECDH('prime256v1');
-  ecdh.generateKeys();
+  var kp = rs.KEYUTIL.generateKeypair('EC', 'secp256r1');
 
-  var pubkey = ecdh.getPublicKey();
-  var privkey = ecdh.getPrivateKey();
+  var pubkey = Buffer.from(kp.pubKeyObj.pubKeyHex, 'hex');
+  var privkey = Buffer.from(kp.prvKeyObj.prvKeyHex, 'hex');
   var privateKey = new ECDSA({
     d: privkey,
     x: pubkey.slice(1, 1 + curveLength),
     y: pubkey.slice(1 + curveLength)
   });
-
   var userPublicKey = pubkey;
 
   // 内部管理用のアプリケーションIDの決定
   console.log('application_id='+ total_application_id);
 
   // KeyHandleの作成
-  var keyHandle = Buffer.concat([Buffer.from([(total_application_id >> 24) & 0xff, (total_application_id >> 16) & 0xff, (total_application_id >> 8) & 0xff, total_application_id & 0xff]), privkey] );
+  var keyHandle = Buffer.concat([Buffer.from([(total_application_id >> 24) & 0xff, (total_application_id >> 16) & 0xff, (total_application_id >> 8) & 0xff, total_application_id & 0xff]),  				privkey] );
   total_application_id++;
   var keyLength = Buffer.from([keyHandle.length]);
 
   // X.509証明書の作成
-  var attestationCert = await x509.fromJwk(
-    privateKey.asPublic().toJWK(),
-    privateKey.toJWK(),
-    'der',
-    {
-      signature: 'ecdsa-with-sha256',
-      days: FIDO_EXPIRE,
-      issuer: { commonName: FIDO_ISSUER },
-      subject: { commonName: FIDO_SUBJECT },
-    },
-  );
+  var tbsc = new rs.KJUR.asn1.x509.TBSCertificate();
 
+  tbsc.setSerialNumberByParam({'int': 1234});
+  tbsc.setSignatureAlgByParam({'name': 'SHA256withECDSA'});
+  tbsc.setIssuerByParam({'str': "/CN=FT FIDO 0200"});  
+  tbsc.setNotBeforeByParam({'str': "190511235959Z"});
+  tbsc.setNotAfterByParam({'str': "340511235959Z"});
+  tbsc.setSubjectByParam({'str': "/CN=FT FIDO P2000000000000"});  
+  tbsc.setSubjectPublicKey(kp.pubKeyObj);
+
+/*
+  //サブジェクトキー識別子
+  var extSKI = new rs.KJUR.asn1.x509.Extension();
+  extSKI.oid = '2.5.29.14';
+  const ski = rs.KJUR.crypto.Util.hashHex(kp.pubKeyObj.pubKeyHex, 'sha1');
+  const derSKI = new rs.KJUR.asn1.DEROctetString({ hex: ski });
+  extSKI.getExtnValueHex = () => {return derSKI.getEncodedHex() };
+  tbsc.appendExtension(extSKI);
+*/
+
+  // FIDO U2F certificate transports extension
+  var extSKI2 = new rs.KJUR.asn1.x509.Extension();
+  extSKI2.oid = '1.3.6.1.4.1.45724.2.1.1';
+  extSKI2.getExtnValueHex = () => { return "03020640" };
+  tbsc.appendExtension(extSKI2);
+    
+  var cert = new rs.KJUR.asn1.x509.Certificate({'tbscertobj': tbsc, 'prvkeyobj': kp_cert.prvKeyObj });
+  cert.sign();
+  var attestationCert = Buffer.from(cert.hTLV, 'hex');
+  
   // 署名の生成
   var input = Buffer.concat([
     Buffer.from([0x00]),
@@ -134,7 +153,7 @@ async function u2f_authenticate(control, challenge, application, keyHandle){
   })
 
   // 署名回数カウンタの決定
-  total_counter++;
+  ++total_counter;
   console.log('total_counter=' + total_counter);
   var counter = Buffer.from([ (total_counter >> 24) & 0xff, (total_counter >> 16) & 0xff, (total_counter >> 8) & 0xff, total_counter & 0xff ])
 
@@ -149,6 +168,7 @@ async function u2f_authenticate(control, challenge, application, keyHandle){
   sign.update(input);
   var signature = sign.sign(privateKey.toPEM());
 
+  console.log('input(' + input.length + ')=' + input.toString('hex'));
   console.log('sigunature(' + signature.length + ')=' + signature.toString('hex'));
 
   // verify sample code
